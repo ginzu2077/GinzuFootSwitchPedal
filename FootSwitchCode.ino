3720a4d3c9594c99aac04e6fd662b304e1ec00e8
ginzu emulation <ginzu2077@gmail.com>
2022-08-09 22:35:15 +0200

    first update mouse Library middle click pin4

--- FootSwitchCode.ino
+++ FootSwitchCode.ino
@@ -1,22 +1,27 @@
 /* 
- * FootSwitchCode by ginzu
+ * FootSwitchPedalCode by ginzu
  */
  
 #include "Keyboard.h"
+#include "Mouse.h"
 
 
 //declaring button pins
 
 const int buttonPinLeft = 2;          
 const int buttonPinRight = 3;  
+const int buttonPinLeftByMouse = 4; 
+
+ 
 void setup() {
 
-  //declare the buttons as input_pullup
-  
-Keyboard.begin();
+//declare the buttons as input_pullup
+
 pinMode(2, INPUT_PULLUP); // Left
 pinMode(3, INPUT_PULLUP); // Right
-
+pinMode(4, INPUT_PULLUP); // Mouse
+Keyboard.begin();
+Mouse.begin();
 }
 
 
@@ -24,12 +29,13 @@ void loop() {
 
   //checking the state of the button
 
-  int joystickLeft = digitalRead(2);
-  int joystickRight = digitalRead(3);
+  int pedalLeft = digitalRead(2);
+  int pedalRight = digitalRead(3);
+  int pedalLeft2 = digitalRead(4);
 
 
     // Pedal Left - Arrow Left Key
-    if (joystickLeft == LOW) {
+    if (pedalLeft == LOW) {
     Keyboard.press(216);
     }
     else {
@@ -37,12 +43,19 @@ void loop() {
     }
     
     // Pedal Right - Arrow Right Key
-    if (joystickRight == LOW) {
+    if (pedalRight == LOW) {
     Keyboard.press(215);
     }
     else {
     Keyboard.release(215);
     }
-
-
+    
+   if (pedalLeft2 == LOW) {
+    Mouse.press(MOUSE_MIDDLE);
+    delay(100);
+   }
+   else {
+   Mouse.release(MOUSE_MIDDLE);
+   delay(100);
+    }
 }
